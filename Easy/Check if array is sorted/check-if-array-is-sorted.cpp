c0913c1f0ea75fb53969ad1c17d968bb0648e9ff
Prince Katare <75197980+princekatare22@users.noreply.github.com>
2023-02-19 12:23:28 +0530

    Updated Solution - GfG to GitHub

--- Easy/Check if array is sorted/check-if-array-is-sorted.cpp
+++ Easy/Check if array is sorted/check-if-array-is-sorted.cpp
@@ -10,7 +10,7 @@ using namespace std;
 class Solution {
   public:
     bool arraySortedOrNot(int arr[], int n) {
-        for(int i=0 ; i<n-1 ; i++)
+        for(int i=0;i<n-1;i++)
         {
             if(arr[i] > arr[i+1]) return false;
         }
